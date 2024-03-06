76c15876360feba15c73b3afa4f0fd6c18bd50d5
thesiyadiya <141635954+thesiyadiya@users.noreply.github.com>
2024-03-06 15:41:15 +0530

    Add files via upload

---
+++ registerpage.php
@@ -0,0 +1,37 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<div class="container mt-1">
+	<form action="login page.php" method="POST">
+		<input type="text" name="txtfnm" placeholder="Enter fullname" class="form-contron mt-1" required></br>
+		<input type="text" name="mb" placeholder="Enter Mobilenumber" class="form-contron mt-1" required></br>
+		<input type="text" name="txtunm" placeholder="Enter Username" class="form-contron mt-1" required></br>
+		<input type="password" name="txtpwd" placeholder="Enter password" class="form-control mt-1" reqruired></br>
+		<input type="submit" value="Log In" class="btn btn-primary w-100">
+	</form>
+</div>
+<?php
+	$con=mysqli_connect("localhost","root","","mail");
+	if(isset($_post(['txtfnm']))
+	{
+		$fnm=$_POST['txtfnm'];
+		$unm=$_POST['txtunm'];
+		$pwd=$_POST['txtpwd'];
+		$mb=$_POST['txmb'];
+		$sql="INSERT INTO `user_login`(`id`, `Full name`, `Mobile number`, `User name`, `Password`) VALUES ('','[txtfnm]','[txtmb]','[textunm]','[txtpwd]')";
+		$res=mysqli_query($con,$sql);
+		$count=mysqli_num_rows($res);
+		if($coun == 1)
+		{
+				
+		}
+		else
+		{
+			echo"Invalid";
+		}
+	}
+?>
+	
+		
+		
+		
+	
