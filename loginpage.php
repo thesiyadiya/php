441c0b66c47c67c112c9b9f983368df8c110d756
thesiyadiya <141635954+thesiyadiya@users.noreply.github.com>
2024-04-06 12:58:57 +0530

    Add files via upload

--- loginpage.php
+++ loginpage.php
@@ -20,3 +20,27 @@
 			
 	}
 ?>
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="diya";
+$conn=mysqli_connect($servername,$username,$password,$database );
+if(!$conn){
+	die("sorry we failed to connect: ".mysqli_connect_error());
+}
+else{
+	echo "connection was succesfull<br>";
+}
+
+$sql="SELECT * FROM `loginpage` ";
+$result=mysqli_query($conn,$sql);
+$num=mysqli_num_rows($result);
+echo $num;
+if($num>0){
+	while($row=mysqli_fetch_assoc($result)){
+		echo var_dump($row);
+		echo "<br>";
+	}
+}
+?>
