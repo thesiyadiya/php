f80be1d154d1d7ba186e3bece63737b15cffd564
thesiyadiya <141635954+thesiyadiya@users.noreply.github.com>
2024-04-10 12:52:03 +0530

    Add files via upload

--- loginpage.php
+++ loginpage.php
@@ -5,42 +5,24 @@
 		<input type="text" name="txtunm" placeholder="Enter username" class="form-control mt-3" required>
 		<input type="password" name="txtpwd" placeholder="Enter password" class="form-control mt-1"required>
 		<input type="submit" value="Log-In" class="btn btn-primary w-100">
-	<center><a href="registerpage.php"> new User? Click Hera To Register</a></center>
+	<center><a href="registerpage2.php"> new User? Click Hera To Register</a></center>
 	</form>
 </div>
 <?php 
 	$con=mysqli_connect("localhost","root","","diya");
 	if(isset($_POST['txtunm']))
 	{
-			$unm=$_POST['txtunm'];
-			
-			$pwd=$_POST['txtpwd'];
-			$sql="INSERT INTO `loginpage`( `user name`, `password`) VALUES ('$unm','$pwd')";
+			$txtunm=$_POST['txtunm'];
+			$txtpwd=$_POST['txtpwd'];
+			$sql="INSERT INTO `login`(`username`,`password`) VALUES ('$txtunm','$txtpwd')";
 			$res=mysqli_query($con,$sql);
+			if($res == true)
+			{
+				echo"yes";
+			}
+			else{
+				echo"no";
+			}
 			
 	}
-?>
-<?php
-$servername="localhost";
-$username="root";
-$password="";
-$database="diya";
-$conn=mysqli_connect($servername,$username,$password,$database );
-if(!$conn){
-	die("sorry we failed to connect: ".mysqli_connect_error());
-}
-else{
-	echo "connection was succesfull<br>";
-}
-
-$sql="SELECT * FROM `loginpage` ";
-$result=mysqli_query($conn,$sql);
-$num=mysqli_num_rows($result);
-echo $num;
-if($num>0){
-	while($row=mysqli_fetch_assoc($result)){
-		echo var_dump($row);
-		echo "<br>";
-	}
-}
 ?>
