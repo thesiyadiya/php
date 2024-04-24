913a0a331027644c9fa0654eed3d36a719a4a89b
thesiyadiya <141635954+thesiyadiya@users.noreply.github.com>
2024-04-24 16:50:52 +0530

    Add files via upload

---
+++ jquery(keypress).php
@@ -0,0 +1,12 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" > 
+</script>
+<input type="text">
+<script>
+	$(document).ready(function()
+	{
+		$('input').keypress(function()
+		{
+			alert("");
+		});
+	});
+</script>
---
+++ jquey(dblclick).php
@@ -0,0 +1,12 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" > 
+</script>
+<input type="button" value="button" value="button">
+<script>
+	$(document).ready(function()
+	{
+		$('input').dblclick(function()
+		{
+			alert("");
+		});
+	});
+</script>
