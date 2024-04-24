7fa347f646c1ddbe5f96564dd98483aedd1b84f1
thesiyadiya <141635954+thesiyadiya@users.noreply.github.com>
2024-04-24 17:04:44 +0530

    Add files via upload

---
+++ jquery(submit).php
@@ -0,0 +1,13 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" > </script>
+<form>
+<input type="submit">
+</form>
+<script>
+	$(document).ready(function()
+	{
+		$('form').submit(function()
+		{
+		alert("");
+		});
+	});
+</script>
