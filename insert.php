862a4a43e1ad2049dc270ee612a82642cf335093
thesiyadiya <141635954+thesiyadiya@users.noreply.github.com>
2024-08-16 13:25:39 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,22 @@
+<form action="select.php" method="POST">
+<input type="text" name="txtnm" placeholder="Enter name">
+<input type="text" name="txtpw" placeholder="Enter password">
+<input type="submit" value="submit" >
+</form>
+<?php
+$conn=mysqli_connect("localhost","root","","diya");
+if($conn->connect_error){
+	die("sorry we failed to conncet:".$conn->connect_error);
+}
+else
+{
+	echo"connection  was succesfull<br>";
+}
+if(isset($_POST['txtnm']))
+{
+	$nm=$_POST['txtnm'];
+	$pw=$_POST['txtpw'];
+	$sql="INSERT INTO `user`( `name`, `password`) VALUES ('$nm','$pw')";
+	$res=mysqli_query($conn,$sql);
+}
+?>
---
+++ select.php
@@ -0,0 +1,14 @@
+<?php
+$conn=mysqli_connect("localhost","root","","diya");
+	$sql="SELECT  `name`, `password` FROM `user` ";
+	$res=mysqli_query($conn,$sql);
+	$num=mysqli_num_rows($res);
+	echo $num."<br>";
+	if($num>0)
+	{
+		while($row=mysqli_fetch_assoc($res))
+		{
+			echo var_dump($row)."<br>";
+		}
+	}
+?>
